9b44948bf200cd53d7e21e0a382a9f5af4926b95
MisterCxmpy <cosgunhussein@gmail.com>
2023-08-21 16:45:18 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 import Media from './components/Media';
+import icon from "./assets/icon.png"
 
 function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Ash's Pawsome Gallary</h1>
+      <h1 className="title">
+        <img src={icon} alt="" />
+        Ash's Pawsome Gallary
+      </h1>
       <Media />
     </div>
   );
