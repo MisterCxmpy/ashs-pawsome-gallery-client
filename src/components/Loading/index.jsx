8b2dda197826f665818178302a4c2f1f7a01e361
MisterCxmpy <cosgunhussein@gmail.com>
2023-08-23 09:34:05 +0100

    add warmup message

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,9 +3,12 @@ import styles from './index.module.css'
 
 export default function Loading() {
   return (
-    <div className={styles['loading-container']}>
-      <div className={styles['loading']}></div>
-      <div className={styles['loading-text']}>loading</div>
-    </div>
+    <>
+      <div className={styles['loading-container']}>
+        <div className={styles['loading']}></div>
+        <div className={styles['loading-text']}>loading</div>
+      </div>
+      <h3 className={styles['warmup-message']}>API is warming up.</h3>
+    </>
   )
 }
